55217c438d31c3792a324967b1d53690019647cf
Bernit-Chen <bernit1213@gmail.com>
2024-03-25 19:09:35 +0800

    3_passed10

--- hw1/src/Classroom/Classroom.sol
+++ hw1/src/Classroom/Classroom.sol
@@ -4,9 +4,14 @@ pragma solidity ^0.8.0;
 /* Problem 1 Interface & Contract */
 contract StudentV1 {
     // Note: You can declare some state variable
-
+    bool check_v1=true;
     function register() external returns (uint256) {
         // TODO: please add your implementaiton here
+        if(check_v1==true) {
+            check_v1=false;
+            return 1000;
+        }
+        else return 123;
     }
 }
 
@@ -18,6 +23,9 @@ interface IClassroomV2 {
 contract StudentV2 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        bool check_v2=IClassroomV2(msg.sender).isEnrolled();
+        if(check_v2==false) return 1000;
+        else return 123;
     }
 }
 
@@ -25,5 +33,7 @@ contract StudentV2 {
 contract StudentV3 {
     function register() external view returns (uint256) {
         // TODO: please add your implementaiton here
+        if(gasleft()>7000) return 1000;
+        else return 123;
     }
 }
